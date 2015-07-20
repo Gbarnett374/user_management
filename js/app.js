a9d8e519faaf1089c297d0da5fbe0f23226e439f
gbarnett374 <gbarnett@harryfox.com>
2015-07-19 20:03:53 -0400

    created a service, added some functions to the controller.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,27 @@
 var app = angular.module('userManagement', [] );
+
+app.service('users', ['$http', function ($http) {
+
+	var base_url = '/controllers/user_controller.php';
+
+	this.getAllUsers = function () {
+		return $http.get(base_url);
+	}
+
+	this.getUser = function ($user_id) {
+		return $http.get(base_url + '?id=' + user_id)
+	}
+
+	this.addUser = function ($user) {
+		return $http.post(base_url, $user, 'addUser');
+	}
+
+	this.updateUser = function ($user) {
+		return $http.put(base_url, $user, 'updateUser');
+	}
+
+	this.setInactive = function ($user_id) {
+		return $http.put(base_url, $user_id, 'setInactive' );
+	}
+
+}]);
--- js/userController.js
+++ js/userController.js
@@ -1,3 +1,21 @@
-app.controller('userController', function($scope) {
+app.controller('userController', function($scope, users) {
+
+	function getUsers () {
+		//call the the service and make ajax call to get all users.
+		$scope.users = users.getAllUsers(); 
+	}
 	
+	$scope.createUser = function() {
+		// insert a new user to the database.
+	}
+
+	$scope.updateUser = function(){
+		//update user in the database.
+	}
+
+	$scope.setInactive = function(){
+		//sets user as inactive in the db. 
+	}
+
+	getUsers();
 });
